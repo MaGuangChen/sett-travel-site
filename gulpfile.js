ec00866bd14f01fc983d3eff60ac45f1f05b2c5d
paul <kwn791122@gmail.com>
2017-04-18 10:53:24 +0800

    write the gulpfile.js and made the gulp task and install a lot of npm package and use postcss to autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+//呼叫這些npm套件
 var gulp = require('gulp'),
-watch = require('gulp-watch');
-
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 //預設任務
 gulp.task(/*這是任務名稱*/'default',/*這是執行的任務*/function(){
@@ -15,7 +19,10 @@ gulp.task('html',function(){
 //css任務
 gulp.task('styles',function(){
  
- console.log('some scss or post css');
+return gulp.src('./app/assets/styles/styles.css')//灌水的起點
+       .pipe(postcss([cssvars,nested,autoprefixer]))//過濾器
+       .pipe(gulp.dest('./app/temp/styles'));//灌水的目標
+
 
 });
 
